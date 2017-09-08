6a3329e878b83eec5e540c5beb2b157dcd47ac57
Kira Loo <youhavebeenkiraed@gmail.com>
2017-09-08 15:21:02 -0700

    add initial files.

---
+++ JS/calculator.js
@@ -0,0 +1,16 @@
+export class calculatorLogic {
+
+  constructor(birthDay, toDay) {
+    this.birthDay = birthDay;
+    this.toDay = toDay;
+  }
+
+secondsConversion(birthDay, toDay){
+  let toDayTime = toDay.getTime();
+  let birthDayTime = birthDay.getTime();
+  let between = toDayTime - birthDayTime;
+  let total = Math.floor(between / 1000);
+  return total;
+}
+  
+}
